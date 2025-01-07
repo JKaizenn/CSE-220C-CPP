9119d63e8842b0824b1a1da51ba2b5d166326922
Jessen Forbush <for21011@byui.edu>
2025-01-06 17:12:59 -0700

    Initial Commit

---
+++ Week01 Task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Week01 Task
+//
+//  Created by Jessen Forbush on 1/6/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
